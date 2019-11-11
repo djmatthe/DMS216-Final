5c3329c3e8c28eeaaf7da50546f4773238dbc1fd
Darren Matthew <darren.j.matthew@gmail.com>
2019-11-11 12:49:59 -0500

    added a cool font

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { subscribe } from 'mqtt-react';
 import { Player } from '../components'
+import styled from 'styled-components'
+
+const Player1 = subscribe({topic: 'Player1'})(Player)
+const Player2 = subscribe({topic: 'Player2'})(Player)
+const Player3 = subscribe({topic: 'Player3'})(Player)
+const Player4 = subscribe({topic: 'Player4'})(Player)
+
+const Title = styled.div`
+    font-family: Jumbo Regular;
+    font-size: 80px; 
+`
 
 class Lobby extends Component {
-    render(){
+    render() {
         return (
             <div>
-                This is the lobby
-                <Player color="red" playerNum={1}/>
+                <Title>
+                    Press the button on your armband to join the game!
+                </Title>
+                <Player1 color="red" playerNum={1}/>
+                <Player2 color="blue" playerNum={2}/>
+                <Player3 color="green" playerNum={3}/>
+                <Player4 color="yellow" playerNum={4}/>
             </div>
         )
     }
